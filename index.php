e7d51e515ca7019245fd450508f132e6b506cdea
Povilas <povilas06@gmail.com>
2015-03-08 00:53:21 +0000

    Removed old index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-define('BASEDIR', __DIR__.'/../');
-require_once(BASEDIR.'classes/AutoLoader.php');
-
-$dispatcher = new Dispatcher();
-$dispatcher->run();
